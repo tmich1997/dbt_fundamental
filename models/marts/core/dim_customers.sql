3ec0ec5e06c7a9e58a89c1699eee1b6069a04a38
Timble Michael <timble.michael@mip.com.au>
2023-11-22 04:05:09 +0000

    commit

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,51 +1,42 @@
-with customers as (
+with
+    customers as (select * from {{ ref("stg_customers") }}),
 
-    select *
-    from {{ ref('stg_customers') }}
-),
+    orders as (select * from {{ ref("stg_orders") }}),
 
-orders as (
-    select *
-    from {{ ref('stg_orders') }}
-),
+    payments as (select * from {{ ref("stg_payments") }}),
 
-payments as (
-    select *
-    from {{ ref('stg_payments') }}
-),
+    customer_orders as (
 
-customer_orders as (
+        select
+            customer_id,
+            min(order_date) as first_order_date,
+            max(order_date) as most_recent_order_date,
+            count(order_id) as number_of_orders,
+            sum(amount) as lifetime_value
 
-    select
-        customer_id,
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders,
-        sum(amount) as lifetime_value
+        from orders
+        left join payments using (order_id)
 
-    from orders
-    left join payments using (order_id)
+        group by 1
 
-    group by 1
+    ),
 
-),
+    final as (
 
-final as (
+        select
+            customers.customer_id,
+            customers.first_name,
+            customers.last_name,
+            customer_orders.first_order_date,
+            customer_orders.most_recent_order_date,
+            coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+            customer_orders.lifetime_value
 
-    select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-        customer_orders.lifetime_value
+        from customers
 
-    from customers
+        left join customer_orders using (customer_id)
 
-    left join customer_orders using (customer_id)
-
-)
+    )
 
 select *
 from final
